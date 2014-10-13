a0d3350644a3d5c88f87c98bf1923bf4e970a458
tudou0digua <binchen0408002@163.com>
2014-10-13 20:09:00 +0800

    third time progress 1/3

--- src/com/cb/potatoclock/LaunchFragment.java
+++ src/com/cb/potatoclock/LaunchFragment.java
@@ -3,7 +3,6 @@ package com.cb.potatoclock;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
@@ -15,6 +14,9 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class LaunchFragment extends Fragment {
 
@@ -22,13 +24,17 @@ public class LaunchFragment extends Fragment {
 	private LinearLayout linearLayoutWork, linearLayoutShortTime,linearLayoutLongTime;
 	private Button dialogCancel, dialogConfirm;
 	private ImageButton launch;
-	private FragmentCallBack fragmentCallback;
-	
+	private FragmentCallBack fragmentCallBack;
+	private SeekBar seekBarWorkTime,seekBarShortRestTime,seekBarLongRestTime;
+	private TextView tvWorkTime,tvShortRestTime,tvLongRestTime;
+	private TextView tvLaunchWorkTime,tvLaunchShortRestTime,tvLaunchLongRestTime;
+	private int workTime,shortRestTime,longRestTime;
+	private Bundle cacheTime= new Bundle();
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
 		try{
-			fragmentCallback = (MainActivity)activity;
+			fragmentCallBack = (MainActivity)activity;
 		}catch(ClassCastException e){
 			throw new ClassCastException(activity.toString() + "must implements FragmentCallback Interface");
 		}
@@ -38,6 +44,20 @@ public class LaunchFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.launch,container,false);
+		//通过回调，设置workTime,shortRestTime,longRestTime
+		workTime = fragmentCallBack.getWorkTimeValue();
+		shortRestTime = fragmentCallBack.getShortRestTimeValue();
+		longRestTime = fragmentCallBack.getLongRestTimeValue();
+		//设置cacheTime初值
+		cacheTime.putInt("work", workTime);
+		cacheTime.putInt("shortrest", shortRestTime);
+		cacheTime.putInt("longrest", longRestTime);
+		//绑定launch页面的3个显示时间的TextView
+		tvLaunchWorkTime = (TextView)view.findViewById(R.id.textview_worktime);
+		tvLaunchShortRestTime = (TextView)view.findViewById(R.id.textview_shortresttime);
+		tvLaunchLongRestTime = (TextView)view.findViewById(R.id.textview_longresttime);
+		//设置launch页面的3个显示时间的TextView的初始值
+		setLuanchTimeTextView(workTime, shortRestTime, longRestTime);
 		// 生成设置时间的Dialog
 		myDialog();
 		//绑定开始页面的3个LinearLayout
@@ -49,8 +69,9 @@ public class LaunchFragment extends Fragment {
 		linearLayoutShortTime.setOnClickListener(new LinearLayoutListener());
 		linearLayoutLongTime.setOnClickListener(new LinearLayoutListener());
 		//开始番茄工作法，监听launch按钮
-		setLaunch((ImageButton)view.findViewById(R.id.ready_go));
-		fragmentCallback.launchOnClickListener(getLaunch());
+		launch = ((ImageButton)view.findViewById(R.id.ready_go));
+		fragmentCallBack.launchOnClickListener(launch);
+		
 		
 		return view;
 	}
@@ -65,11 +86,13 @@ public class LaunchFragment extends Fragment {
 			WindowManager.LayoutParams layoutParam = window.getAttributes();
 			DisplayMetrics dm = new DisplayMetrics();
 			getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-			// 将dialog的宽设为，屏幕宽的90%
+			// 将Dialog的宽设为，屏幕宽的90%
 			layoutParam.width = (int) (dm.widthPixels * 0.9);
 			// 设置自定义好的dialog窗口属性
 			window.setAttributes(layoutParam);
-			// 在当前dialog window 下绑定Button
+			// 显示Dialog
+			dialogSetDuration.show();
+			// 在当前Dialog window 下绑定Button
 			// 设置Dialog 的取消按钮
 			dialogCancel = (Button) window.findViewById(R.id.dialog_button_cancel);
 			dialogCancel.setOnClickListener(new OnClickListener() {
@@ -83,27 +106,88 @@ public class LaunchFragment extends Fragment {
 			dialogConfirm.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
+					//取出cacheTime中的数据
+					workTime = cacheTime.getInt("work");
+					MainActivity.WORK_TIME = workTime;
+					shortRestTime = cacheTime.getInt("shortrest");
+					MainActivity.SHORT_REST_TIME = shortRestTime;
+					longRestTime = cacheTime.getInt("longrest");
+					MainActivity.LONG_REST_TIME = longRestTime;
+					//设置luanch页面中的有关时间的TextView
+					setLuanchTimeTextView(workTime,shortRestTime,longRestTime);
 					dialogSetDuration.dismiss();
 				}
 			});
-			// 显示Dialog
-			dialogSetDuration.show();
+			//绑定dialog中显示时间的TextView
+			tvWorkTime = (TextView)window.findViewById(R.id.tvWorkTime);
+			tvShortRestTime = (TextView)window.findViewById(R.id.tvShortRestTime);
+			tvLongRestTime = (TextView)window.findViewById(R.id.tvLongRestTime);
+			//设定dialog中显示时间的TextView的初始值
+			setDialogTimeTextView(workTime, shortRestTime, longRestTime);
+			//绑定Dialog中3个SeekBar，并监听
+			seekBarWorkTime = (SeekBar)window.findViewById(R.id.seekBarWorkTime);
+			seekBarShortRestTime = (SeekBar)window.findViewById(R.id.seekBarShortRestTime);
+			seekBarLongRestTime = (SeekBar)window.findViewById(R.id.seekBarLongRestTime);
+			//设置3个seekBar的初始值
+			seekBarWorkTime.setProgress(workTime-1);
+			seekBarShortRestTime.setProgress(shortRestTime-1);
+			seekBarLongRestTime.setProgress(longRestTime-1);
+			//生成OnSeekBarChangeListener
+			SeekBarListener seekBarListener = new SeekBarListener();
+			seekBarWorkTime.setOnSeekBarChangeListener(seekBarListener);
+			seekBarShortRestTime.setOnSeekBarChangeListener(seekBarListener);
+			seekBarLongRestTime.setOnSeekBarChangeListener(seekBarListener);
+			
 		}
 	}
+	//实现OnSeekBarChangeListener接口，监听Dialog中3个SeekBar
+	public class SeekBarListener implements OnSeekBarChangeListener{
 
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser) {
+			int time = progress+1;
+			switch(seekBar.getId()){
+			case R.id.seekBarWorkTime:
+				tvWorkTime.setText(time+"");
+				cacheTime.putInt("work", time);
+				break;
+			case R.id.seekBarShortRestTime:
+				tvShortRestTime.setText(time+"");
+				cacheTime.putInt("shortrest", time);
+				break;
+			case R.id.seekBarLongRestTime:
+				tvLongRestTime.setText(time+"");
+				cacheTime.putInt("longrest", time);
+				break;
+			}
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+		}
+		
+	}
 	// 设置工作时间等的Dialog
 	private void myDialog() {
 		dialogSetDuration = new Dialog(getActivity(),R.style.dialogSetDuration);
 		dialogSetDuration.setContentView(R.layout.dialog_setduration);
 	}
-
-	public ImageButton getLaunch() {
-		return launch;
+	//设置luanch页面有关时间的TextView
+	private void setLuanchTimeTextView(int work,int shortrest,int longrest){
+		tvLaunchWorkTime.setText(work+"分钟");
+		tvLaunchShortRestTime.setText(shortrest+"分钟");
+		tvLaunchLongRestTime.setText(longrest+"分钟");
 	}
-
-	public void setLaunch(ImageButton launch) {
-		this.launch = launch;
+	//设置dialog页面有关时间的TextView
+	private void setDialogTimeTextView(int work,int shortrest,int longrest){
+		tvWorkTime.setText(String.valueOf(work));
+		tvShortRestTime.setText(String.valueOf(shortrest));
+		tvLongRestTime.setText(String.valueOf(longrest));	
 	}
-	
 
 }
--- src/com/cb/potatoclock/MainActivity.java
+++ src/com/cb/potatoclock/MainActivity.java
@@ -3,9 +3,12 @@ package com.cb.potatoclock;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 
@@ -13,6 +16,14 @@ public class MainActivity extends Activity implements FragmentCallBack{
 
 	private LaunchFragment launchFragment;
 	private FragmentManager fragmentManager;
+	public static int WORK_TIME = 25;
+	public static int LONG_REST_TIME = 25;
+	public static int SHORT_REST_TIME = 5;
+	private final int WORKING = 0;
+	private final int SHORT_REST = 1;
+	private final int LONG_REST = 2;
+	public int WORKING_TYPE = WORKING;
+	public int POTATO_NUMBER = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +38,7 @@ public class MainActivity extends Activity implements FragmentCallBack{
 		ft.commit();
 		
 	}
-
+	//回调函数，实现LaunchFragment页面launch按钮的监听
 	@Override
 	public void launchOnClickListener(View view) {
 		
@@ -36,27 +47,86 @@ public class MainActivity extends Activity implements FragmentCallBack{
 			@Override
 			public void onClick(View v) {
 				FragmentTransaction ft = fragmentManager.beginTransaction();
-				ft.add(R.id.launchFrameLayout,new WorkingFragment());
+				ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+				ft.replace(R.id.launchFrameLayout,new WorkingFragment());
 				ft.addToBackStack(null);
 				ft.commit();
 			}
 		});
+	}
+	//回调函数，监听WokingFragment倒计时完成按钮
+	@Override
+	public void workTimesUpButtonListener(View view) {
+		view.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FragmentTransaction ft = fragmentManager.beginTransaction();
+				ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+				if(WORKING_TYPE == WORKING){
+					if(POTATO_NUMBER%4 == 0){
+						WORKING_TYPE = LONG_REST;
+					}else{
+						WORKING_TYPE = SHORT_REST;
+					}
+					POTATO_NUMBER++;
+				}else{
+					WORKING_TYPE = WORKING;
+				}
+				
+				ft.replace(R.id.launchFrameLayout,new WorkingFragment());
+				
+				Log.d("PotatoNumber", POTATO_NUMBER+"");
+				ft.addToBackStack(null);
+				ft.commit();
+			}
+		});
+	}
+	//回调函数，监听WokingFragment取消番茄时钟按钮
+	@Override
+	public void stopPotatoClockButtonListener(View view) {
+		// TODO Auto-generated method stub
 		
-//		LaunchFragment launchFrag = (LaunchFragment)fragmentManager.findFragmentById(R.id.launchFrameLayout);
-//		if(launchFrag != null){
-//			ImageButton launch = launchFrag.getLaunch();
-//			launch.setOnClickListener(new OnClickListener() {
-//				
-//				@Override
-//				public void onClick(View v) {
-//					FragmentTransaction ft = fragmentManager.beginTransaction();
-//					ft.add(R.id.launchFrameLayout,new WorkingFragment());
-//					ft.addToBackStack(null);
-//					ft.commit();
-//				}
-//			});
-//		}
+	}
+	
+	@Override
+	public int getWorkTimeValue() {
+		return WORK_TIME;
 	}
 
+	@Override
+	public int getShortRestTimeValue() {
+		return SHORT_REST_TIME;
+	}
 
+	@Override
+	public int getLongRestTimeValue() {
+		return LONG_REST_TIME;
+	}
+	//回调函数，初始化WorkingFragment界面
+	@Override
+	public int initWorkingFragment(View view, Button button) {
+		
+		switch(WORKING_TYPE){
+		case WORKING:
+			view.setBackgroundColor(Color.parseColor("#FF0033"));
+			if(POTATO_NUMBER%4 == 0){
+				button.setText("长休息");
+			}else{
+				button.setText("短休息");
+			}
+			return WORK_TIME;
+		case SHORT_REST:
+			view.setBackgroundColor(Color.parseColor("#CCFF66"));
+			button.setText("工作");
+			return SHORT_REST_TIME;
+		case LONG_REST:
+			view.setBackgroundColor(Color.parseColor("#99CC33"));
+			button.setText("工作");
+			return LONG_REST_TIME;
+		default:
+			return 100;
+		}
+
+	}
 }
